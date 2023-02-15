f54543e71cb4153323d3594e721cccff7d885c8d
Vincent <vincent.hlaka@sambeconsulting.com>
2023-02-15 11:28:14 +0200

    Dictionaries section added

---
+++ PythonBootcamp/Dictionaries/WorkingWithDictionaries.py
@@ -0,0 +1,10 @@
+
+
+# Given
+song = {'artist': 'John Lennon', 'name': 'Imagine'}
+
+# 1. Adding a new key: value pair to the dictionary. The key is 'year' and the value is 1971
+
+song['year'] = 1971
+# 2. Updating the value of the 'artist' key with 'John Winston Ono Lennon'
+song['artist'] = 'John Winston Ono Lennon'
