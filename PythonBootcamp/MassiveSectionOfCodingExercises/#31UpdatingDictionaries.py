ff752eef9282bab6e3e5f1c391d9dc22286b8238
Vincent <vincent.hlaka@sambeconsulting.com>
2023-02-16 11:53:26 +0200

    Massive Section of coding challenges

---
+++ PythonBootcamp/MassiveSectionOfCodingExercises/#31UpdatingDictionaries.py
@@ -0,0 +1,9 @@
+# Ex 31
+
+# Consider the following dictionary:
+phone = {'Brand':'Samsung', 'Price':650.2, 'Seller': 'Nile'}
+
+# Using a single method add the following key:value pairs to the dictionary:  'OS':'Android' and  'Color': 'Black'
+# code
+
+phone.update({'OS':'Android', 'Color': 'Black'})
---
+++ PythonBootcamp/MassiveSectionOfCodingExercises/#36InsertingIntoAList.py
@@ -0,0 +1,11 @@
+# Ex 36
+
+days = [10, 11, 13, 14, 15]
+
+# YOUR CODE STARTS HERE
+# Using a list method insert the value 12 between 11 and 13.
+
+## Inserting 12 at index 2
+days.insert(2, 12)
+
+print(days)  # => [10, 11, 12, 13, 14, 15]
