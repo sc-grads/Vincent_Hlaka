b33c7f0467242281db7228cfbc5b19aaccde8f88
Vincent <vincent.hlaka@sambeconsulting.com>
2023-02-16 14:25:04 +0200

    Python Modules

---
+++ PythonBootcamp/WorkingWithPythonModules/ImportingModulesEx57.py
@@ -0,0 +1,10 @@
+# Exercise 57
+
+# Importing the os module
+import os
+
+# Importing math as an alias
+import math as my_math
+
+# Importing everything from random module directly
+from random import *
