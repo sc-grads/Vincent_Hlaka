1f51bdbaeec2a14f95bbbd5b011cdb429e64c980
Vincent <vincent.hlaka@sambeconsulting.com>
2023-02-17 15:24:56 +0200

    Decorators added

---
+++ PythonBootcamp/DecoratorsInPython/InnerFunctionClosures.py
@@ -0,0 +1,16 @@
+# Inner functions. closures and free vars
+
+def outer():
+    msg = 'Python'
+    x = 1
+    def inner():
+        print(f'{msg} is really cool') # msg is a free variable(non-local)
+        nonlocal x
+        x += 1
+        print(x)
+    return inner
+
+fn = outer()
+fn()
+
+print(fn.__code__.co_freevars)
